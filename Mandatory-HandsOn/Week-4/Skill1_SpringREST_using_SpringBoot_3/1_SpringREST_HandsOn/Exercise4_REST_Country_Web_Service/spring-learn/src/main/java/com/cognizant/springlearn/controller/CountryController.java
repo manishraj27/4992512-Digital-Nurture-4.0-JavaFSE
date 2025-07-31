729981cddefc53deea5986b60af9b44a1f32367e
manishraj27 <manish012raj@gmail.com>
2025-07-31 20:09:05 +0530

    Add Spring RESTful Web Service for Hello World and Country details
    - Created a new Spring Boot application with RESTful capabilities.
    - Added `pom.xml` for Maven dependencies including Spring Boot Starter Web and DevTools.
    - Implemented `SpringLearnApplication` as the main application class.
    - Developed `HelloController` to handle GET requests at "/hello" endpoint, returning a simple greeting.
    - Configured application properties to set application name and server port.
    - Created a test class `SpringLearnApplicationTests` to verify application context loading.
    - Added a new RESTful service to fetch country details.
    - Implemented `CountryController` to return country information from an XML configuration.
    - Defined `Country` model class to represent country data.
    - Created `country.xml` for Spring bean configuration of country details.
    - Updated test cases to ensure proper functionality of the new features.

---
+++ Mandatory-HandsOn/Week-4/Skill1_SpringREST_using_SpringBoot_3/1_SpringREST_HandsOn/Exercise4_REST_Country_Web_Service/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,26 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        LOGGER.info("START - getCountryIndia()");
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = (Country) context.getBean("in");
+        context.close();
+
+        LOGGER.info("END - getCountryIndia()");
+        return country;
+    }
+}
