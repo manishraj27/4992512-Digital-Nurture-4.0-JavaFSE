1e774359df0b89df8b06c5beacd8b0ffb50b48e2
manishraj27 <manish012raj@gmail.com>
2025-08-05 08:34:47 +0530

    Week-6 assignments done

---
+++ Mandatory-HandsOn/Week-6/Skill1_React/Exercise3/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+;
+
+function App() {
+  return (
+   <div>
+    <CalculateScore Name={"Manish Raj"}
+      School={"ABC High School"}
+      total={284}
+      goal={3} />
+   </div>
+  );
+}
+
+export default App;
