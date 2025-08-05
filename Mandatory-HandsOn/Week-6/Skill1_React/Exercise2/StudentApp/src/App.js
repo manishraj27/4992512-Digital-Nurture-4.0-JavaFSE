dff4280278447bb7109f87af05c26947f69cad9b
manishraj27 <manish012raj@gmail.com>
2025-08-05 08:12:46 +0530

    Initialize Student Management System React App with essential files and components
    - Created package.json with dependencies for React and testing libraries.
    - Added public assets: favicon.ico, logo192.png, logo512.png, manifest.json, and robots.txt.
    - Implemented index.html as the main HTML template for the app.
    - Developed main App component integrating Home, About, and Contact components.
    - Created individual components for Home, About, and Contact pages with basic content.
    - Added CSS styles for the App and index.
    - Set up testing framework with initial test for App component.
    - Included reportWebVitals for performance tracking.
    - Configured setupTests for Jest DOM matchers.

---
+++ Mandatory-HandsOn/Week-6/Skill1_React/Exercise2/StudentApp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import About from './component/About';
+import Contact from './component/Contact';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div className="container">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
